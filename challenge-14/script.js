1c662d06ead361494c43e2cb79d8f239ba0b98f4
billalp <billal.patel@rightmove.co.uk>
2019-12-08 16:38:53 +0000

    Completed

--- challenge-14/script.js
+++ challenge-14/script.js
@@ -1,34 +1,46 @@
 /* CHALLENGE 14 - Object Destructuring
 Create shortPerson() function that will destructure each person object.
 Sample result:
-{n: "Mike", c: "Spain", a: 23, p: 100}
+{n: 'Mike', c: 'Spain', a: 23, p: 100}
 If input object doesn't have postsQuantity field
 it should get default value 0.
 */
 
-"use strict";
+'use strict';
 
-var person1 = {
-  name: "Mike",
+const person1 = {
+  name: 'Mike',
   info: {
-    country: "Spain",
+    country: 'Spain',
     age: 23
   },
   postsQuantity: 100
 };
 
-var person2 = {
-  name: "Alice",
+const person2 = {
+  name: 'Alice',
   info: {
-    country: "Italy",
+    country: 'Italy',
     age: 25
   }
 };
 
-// Write function here
+const shortPerson = (object) => {
+    let {
+        name: n,
+        info: { 
+            country: c, 
+            age: a 
+        },
+        postsQuantity: p = 0
+      } = object;
+    return {
+        n, c, a, p
+      };
+}
 
 console.log(shortPerson(person1));
-// {n: "Mike", c: "Spain", a: 23, p: 100}
+// {n: 'Mike', c: 'Spain', a: 23, p: 100}
 
 console.log(shortPerson(person2));
-// {n: "Alice", c: "Italy", a: 25, p: 0}
+// {n: 'Alice', c: 'Italy', a: 25, p: 0}
