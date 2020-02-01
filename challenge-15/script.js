60e11554bf5223c26e76c9a7633a3ae522d899a1
billalp <billal.patel@rightmove.co.uk>
2020-02-01 18:54:00 +0000

    New challenge setup

---
+++ challenge-15/script.js
@@ -0,0 +1,16 @@
+/* CHALLENGE 15 - Iterate over String
+Count number of the lowercase vowel letters in the string.
+Vowel letters - a, e, i, o, u
+*/
+
+'use strict';
+
+const vowelsCount = 0;
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+const str = 'Today is best day of my life';
+
+// Write code here
+
+console.log(vowelsCount);
+// 8
